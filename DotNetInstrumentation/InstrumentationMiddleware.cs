0bb9f6fb7bdf97f6d3177e4d8822963f664658e3
Justin Welsch <j.welsch@gmail.com>
2019-09-04 20:09:58 -0400

    Initial commit

---
+++ DotNetInstrumentation/InstrumentationMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace DotNetInstrumentation
+{
+    public class InstrumentationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public InstrumentationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
