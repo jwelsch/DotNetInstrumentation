a7b383cb667acea7e83160f3025b8dcf5f2646b6
Justin Welsch <j.welsch@gmail.com>
2019-09-08 20:54:28 -0400

    Add class to manage body statistics

---
+++ DotNetInstrumentation/IBodyStatistics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetInstrumentation
+{
+    public interface IBodyStatistics
+    {
+        long Minimum
+        {
+            get;
+        }
+
+        long Maximum
+        {
+            get;
+        }
+
+        double Average
+        {
+            get;
+        }
+
+        long Total
+        {
+            get;
+        }
+
+        int Count
+        {
+            get;
+        }
+    }
+}
