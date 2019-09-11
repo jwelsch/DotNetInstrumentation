2a8efea4f3d5520b21b6b621e655c8c3bb630445
Justin Welsch <j.welsch@gmail.com>
2019-09-11 08:57:30 -0400

    Add exception to throw when expected HttpContext is not found.

---
+++ DotNetInstrumentation/HttpContextNotFoundException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace DotNetInstrumentation
+{
+
+    [Serializable]
+    public class HttpContextNotFoundException : Exception
+    {
+        public HttpContextNotFoundException()
+        {
+        }
+
+        public HttpContextNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public HttpContextNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected HttpContextNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
