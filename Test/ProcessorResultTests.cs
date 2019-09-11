4eecc2794d741907f8b751715ff7148062bde41a
Justin Welsch <j.welsch@gmail.com>
2019-09-11 09:05:06 -0400

    Add tests for ProcessorResult

---
+++ Test/ProcessorResultTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    [TestClass]
+    public class ProcessorResultTests
+    {
+        [TestMethod]
+        public void PassNullForErrorReportsNoError()
+        {
+            var result = new TestProcessorResult();
+
+            Assert.IsFalse(result.IsError);
+            Assert.IsNull(result.Error);
+        }
+
+        [TestMethod]
+        public void PassNotNullForErrorReportsError()
+        {
+            var result = new TestProcessorResult(new Exception("Sample error message."));
+
+            Assert.IsTrue(result.IsError);
+            Assert.IsInstanceOfType(result.Error, typeof(Exception));
+        }
+    }
+}
---
+++ Test/TestProcessorResult.cs
@@ -0,0 +1,15 @@
+﻿using DotNetInstrumentation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class TestProcessorResult : ProcessorResult
+    {
+        public TestProcessorResult(Exception error = null)
+            : base(error)
+        {
+        }
+    }
+}
